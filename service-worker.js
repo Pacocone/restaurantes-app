bbc82aa3c2165ceb95bbe5306b54856b62a7b7aa
Pacocone <pacoconesa@vaquelia.com>
2025-07-27 18:53:25 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "restaurantes-v1";
+const CACHE_NAME = "restaurantes-v2";
 const urlsToCache = ["index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (event) => {
